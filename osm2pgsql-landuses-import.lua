3ac6dd43f6121453429410b4ae66e2b5ac3e742f
Amanda McCann <amanda@technomancy.org>
2024-02-22 18:35:30 +0100

    misc

---
+++ osm2pgsql-landuses-import.lua
@@ -0,0 +1,32 @@
+local tables = {}
+
+tables.landuse = osm2pgsql.define_table({
+	name = 'landuse',
+	ids = { type = 'any', id_column = 'osm_id', type_column = 'osm_type' },
+	cluster = 'no',
+	columns = {
+		{ column = 'landuse', type = 'text' },
+		{ column = 'geom', type = 'multipolygon', projection = 4326 },
+	},
+	{ indexes = { { column = "landuse", method = "btree" }, } }
+})
+
+function insert_object(object) 
+	local geom = object:as_multipolygon();
+	tables.landuse:insert({
+		landuse = object.tags.landuse,
+		geom = geom,
+	});
+end
+
+function osm2pgsql.process_way(object)
+	if object.tags.landuse and object.is_closed then
+		insert_object(object);
+	end
+end
+
+function osm2pgsql.process_relation(object)
+	if object.tags.landuse then
+		insert_object(object);
+	end
+end
